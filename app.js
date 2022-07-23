e19db2f6c93f3f96ccddbef602aa48cae1448d63
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-07-23 17:36:22 -0400

    security issue

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const mongoDB = process.env.MONGO_DB;
+const mongoDB = `${process.env.MONGO_DB}`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
